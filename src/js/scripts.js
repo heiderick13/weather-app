88e6f770d77fcf61b3c0a6f7ba773e5dd1a5fce3
Marcelo Oliveira <marceloheiderick@outlook.com>
2023-02-16 18:20:40 -0300

    getting data from api

---
+++ src/js/scripts.js
@@ -0,0 +1,53 @@
+const apiKey = "dd9ec81f2d2672d352f3fbc746477d59";
+const countryFlagURL = "https://countryflagsapi.com/png/";
+
+const cityInput = document.querySelector("#city-input");
+const searchBtn = document.querySelector("#search-btn");
+
+const cityName = document.querySelector("#city-name");
+const country = document.querySelector("#country-img");
+const temperature = document.querySelector("#temperature span");
+const weatherType = document.querySelector("#type");
+const typeIcon = document.querySelector("#img-type");
+const humidity = document.querySelector("#humidity span");
+const wind = document.querySelector("#wind span");
+
+const dataContainer = document.querySelector("#city-data");
+
+// Functions
+async function getWeatherData(city) {
+  const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
+
+  const resp = await fetch(apiURL);
+  const data = await resp.json();
+
+  console.log(data);
+  return data;
+}
+
+async function showWeatherData(city) {
+  const data = await getWeatherData(city);
+  let iconId = data.weather[0].icon;
+
+  cityName.textContent = data.name;
+  temperature.textContent = parseInt(data.main.temp);
+  country.setAttribute("src", countryFlagURL + data.sys.country);
+  weatherType.textContent = data.weather[0].description;
+  typeIcon.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${iconId}.png`
+  );
+  humidity.textContent = data.main.humidity + "%";
+  wind.textContent = data.wind.speed + " km/h";
+
+  dataContainer.classList.remove("hide");
+}
+
+// Event handlers
+searchBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  const city = cityInput.value;
+
+  showWeatherData(city);
+});
